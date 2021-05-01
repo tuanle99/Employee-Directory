747eaa66f9bb34e36949cd4a542dd3c2bd1e74cf
tuanhoangle <tuanleprofessional@gmail.com>
2021-05-01 15:59:27 -0400

    home page style

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,9 @@
+/* eslint-disable react/style-prop-object */
 import React, { Component } from "react";
 import API from "../utils/API";
-import Card from "../components/Card";
+import { Card } from "react-bootstrap";
 import Container from "../components/Container";
+import Row from "../components/Row";
 
 class Home extends Component {
   state = {
@@ -19,15 +21,33 @@ class Home extends Component {
     return (
       <Container>
         {console.log(this.state.results)}
-        <div>
-          {this.state.results.map((item) => {
+        <Row>
+          {this.state.results.map((user) => {
             return (
-              <div key={item.id.value}>
-                <Card image={item.picture.large} />
+              <div key={user.id.value}>
+                <Card style={{ width: "18rem" }}>
+                  <Card.Img variant="top" src={user.picture.large} />
+                  <Card.Body>
+                    <Card.Title>
+                      {user.name.first} {user.name.last}
+                    </Card.Title>
+                    <Card.Text>{user.email}</Card.Text>
+                    <Card.Text>Age: {user.dob.age}</Card.Text>
+                    <Card.Text>
+                      Address: {user.location.street.number}{" "}
+                      {user.location.street.name} {user.location.city}{" "}
+                      {user.location.state}, {user.location.country}{" "}
+                      {user.location.postcode}
+                    </Card.Text>
+                    <Card.Text>
+                      Employee ID: {user.id.name} {user.id.value}
+                    </Card.Text>
+                  </Card.Body>
+                </Card>
               </div>
             );
           })}
-        </div>
+        </Row>
       </Container>
     );
   }
